50dcad890b0d5b686b1101471607f22fb10c6f0e
USER <USER@test.com>
2018-03-30 13:52:55 +0900

    Home_test

--- Simple_Calcultor/src/Calculator.java
+++ Simple_Calcultor/src/Calculator.java
@@ -5,7 +5,8 @@ public class Calculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Calculator is initialized");
-		System.out.println("Test_push");
+		System.out.println("Test_push_Home");
+		
 	}
 
 }
